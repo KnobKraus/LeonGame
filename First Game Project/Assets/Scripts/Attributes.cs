d5d460e6fc2bad0f22d87439c919ffb211b08053
Tobias Heinz <to2bihe@gmail.com>
2017-02-06 23:24:21 +0100

    FatZombieEnemy, Attributes-Konzept, alternatives EnemyMovement, EnemyMeeleAttack (TODO: AttackSpeed mit Framezahl regulieren), ZombieSound für MeeleAttak

---
+++ First Game Project/Assets/Scripts/Attributes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes : MonoBehaviour {
+    public int level;
+    public int hitpoints;
+    public float moveSpeed;
+    public int mana;
+    public bool armored;
+    public int meeleDamage;
+    public int meeleRange;
+    public int attackInterval;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hitpoints <= 0){
+            Destroy(gameObject);
+        }
+	}
+}
