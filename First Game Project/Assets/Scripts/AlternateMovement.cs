b51ee4e78d1fdec529df72859ea545a350e0bb3c
Nico-PC\test <nico_steinberg@hotmail.de>
2017-02-07 12:04:50 +0100

    no message

--- First Game Project/Assets/Scripts/AlternateMovement.cs
+++ First Game Project/Assets/Scripts/AlternateMovement.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlternateMovement : MonoBehaviour
-{
+public class AlternateMovement : MonoBehaviour {
     public float moveSpeed;
-
-    // Use this for initialization
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -26,28 +20,23 @@ public class AlternateMovement : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,moveSpeed);
+        }
+
         if (Input.GetKey(KeyCode.S))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,-moveSpeed);
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveSpeed);
-        }
-
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * 80 / 100, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * 90 / 100, GetComponent<Rigidbody2D>().velocity.y);
         }
         if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 80 / 100);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 90 / 100);
         }
-
-        
-
-
-
     }
 }
